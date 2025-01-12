79448c82cf497edb4709e9c82cc08f4ade8dc9cb
Jernej Skrabec <jernej.skrabec@gmail.com>
2025-01-12 21:33:53 +0100

    refactor(allwinner): split cortex-a53 and gicv2 config
    Till now all Allwinner 64-bit SoC were Cortex-A53 and used GICv2. In
    preparation for newer cores with newer GIC split out those settings to
    separate mk file.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- plat/allwinner/common/allwinner-common.mk
+++ plat/allwinner/common/allwinner-common.mk
@@ -6,7 +6,6 @@
 
 include lib/xlat_tables_v2/xlat_tables.mk
 include lib/libfdt/libfdt.mk
-include drivers/arm/gic/v2/gicv2.mk
 
 AW_PLAT			:=	plat/allwinner
 
@@ -20,11 +19,8 @@ PLAT_BL_COMMON_SOURCES	:=	drivers/ti/uart/${ARCH}/16550_console.S	\
 				${AW_PLAT}/common/sunxi_common.c
 
 BL31_SOURCES		+=	drivers/allwinner/axp/common.c		\
-				${GICV2_SOURCES}			\
 				drivers/delay_timer/delay_timer.c	\
 				drivers/delay_timer/generic_delay_timer.c \
-				lib/cpus/${ARCH}/cortex_a53.S		\
-				plat/common/plat_gicv2.c		\
 				plat/common/plat_psci_common.c		\
 				${AW_PLAT}/common/sunxi_bl31_setup.c	\
 				${AW_PLAT}/${PLAT}/sunxi_idle_states.c	\
@@ -86,18 +82,6 @@ endif
 # The bootloader is guaranteed to only run on CPU 0 by the boot ROM.
 COLD_BOOT_SINGLE_CPU		:=	1
 
-# Do not enable SPE (not supported on ARM v8.0).
-ENABLE_SPE_FOR_NS		:=	0
-
-# Do not enable SVE (not supported on ARM v8.0).
-ENABLE_SVE_FOR_NS		:=	0
-
-# Enable workarounds for Cortex-A53 errata. Allwinner uses at least r0p4.
-ERRATA_A53_835769		:=	1
-ERRATA_A53_843419		:=	1
-ERRATA_A53_855873		:=	1
-ERRATA_A53_1530924		:=	1
-
 # The traditional U-Boot load address is 160MB into DRAM.
 PRELOADED_BL33_BASE		?=	0x4a000000
 
@@ -109,6 +93,3 @@ SEPARATE_CODE_AND_RODATA	:=	1
 
 # BL31 gets loaded alongside BL33 (U-Boot) by U-Boot's SPL
 RESET_TO_BL31			:=	1
-
-# This platform is single-cluster and does not require coherency setup.
-WARMBOOT_ENABLE_DCACHE_EARLY	:=	1
